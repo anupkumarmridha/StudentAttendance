e259b4ae3c0fd678aca08587332c5dcd6074175c
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2022-10-27 02:23:49 +0530

    updated

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,5 @@
-from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-import home.models as home_model
 
 # Create your models here.
 class User(AbstractUser):
