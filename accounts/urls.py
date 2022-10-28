ccb9fa538dacde3084d903063fd08d6d8f1dcfc9
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2022-10-28 20:02:28 +0530

    change password working perfectly

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from accounts import views
+from accounts import resetPasswordView
 from accounts import studentView, facultyView
 urlpatterns = [
   path('/signup/', views.handelSingup, name='handelSingup'),
@@ -15,4 +16,10 @@ urlpatterns = [
   path('viewFacultyProfile', facultyView.viewFacultyProfile, name='viewFacultyProfile'),
   path('addFaculty', facultyView.addFaculty, name='addFaculty'),
   path('updateFaculty/<int:pk>', facultyView.updateFaculty.as_view(), name='updateFaculty'),
+
+  # operations regarding password changes
+  path("password_change", resetPasswordView.password_change, name="password_change"),
+  path("password_reset", resetPasswordView.password_reset_request, name="password_reset"),
+  path('reset/<uidb64>/<token>', resetPasswordView.passwordResetConfirm, name='password_reset_confirm'),
+  
 ]
